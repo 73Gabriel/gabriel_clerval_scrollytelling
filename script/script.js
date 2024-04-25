714784571da5d154bbd99b1c72eb39eba1526384
GabrielNBA24 <gabriellolclerval@gmail.com>
2024-04-24 21:03:39 -0400

    update.html.css.js

--- script/script.js
+++ script/script.js
@@ -1,2 +1,21 @@
+const { ScrollTrigger } = window;
 
 
+const feuAnimation = gsap.timeline();
+
+
+feuAnimation.to('.feu', {
+    backgroundPosition: '0 -2000%',
+    duration: 0.8,
+    repeat: -1,
+    ease: 'steps(20)',
+});
+
+// animation du feu on scroll
+ScrollTrigger.create({
+    trigger: '.feu',
+    start: 'top bottom',
+    end: 'bottom top',
+    animation: feuAnimation,
+    scrub: true,
+});
